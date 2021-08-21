3f2f5647e158fb4ff53c296c25192e95ce5b39ad
Andrew <andrewgarcia@uchicago.edu>
2021-08-21 14:42:37 -0500

    Dataset Sorter Works!

--- projects/urbanSoundsSorter.py
+++ projects/urbanSoundsSorter.py
@@ -5,23 +5,24 @@ classes = ['air_conditioner', 'car_horn', 'children_playing',
            'dog_bark', 'drilling', 'engine_idling', 'gun_shot',
            'jackhammer', 'siren', 'street_music']
 
-# Iterates through all the folders in the dataset
-for i in range(1,11):
-    # Source folder path relative to this file
-    sourceFolder = './UrbanSound8K/audio/fold' + str(i) + '/'
-    fileList = os.listdir(sourceFolder)
 
-    # Iterates through all the classes in the dataset
-    for c in range(0,10):
-        # Make destination folder
-        destFolder = './UrbanSound8K/' + classes[c] + '_files'
-        os.mkdir(destFolder)
-        print('Made Folder: ', destFolder)
+# Iterates through all the classes in the dataset
+for c in range(0,10):
+    # Make destination folder
+    destFolder = './archive/UrbanSound8K/' + classes[c] + '_files' + '/'
+    os.mkdir(destFolder)
+    print('Made Folder: ', destFolder)
 
-        # Audio file name format:
-        # [fsID]-[classID]-[occurrenceID]-[sliceID].wav
-        # Matches classID
-        pattern = r'\d+-' + str(c) + r'-\d+-\d+.wav'
+    # Audio file name format:
+    # [fsID]-[classID]-[occurrenceID]-[sliceID].wav
+    # Matches classID
+    pattern = r'\d+-' + str(c) + r'-\d+-\d+.wav'
+
+    # Iterates through all the folders in the dataset
+    for i in range(1,11):
+        # Source folder path relative to this file
+        sourceFolder = './archive/UrbanSound8K/audio/fold' + str(i) + '/'
+        fileList = os.listdir(sourceFolder)
 
         #Iterates through all the files in the folder
         for file in fileList:
