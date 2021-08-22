cdd0d60dda8fa6abe2e006411eafd97685360a21
Andrew <andrewgarcia@uchicago.edu>
2021-08-22 13:04:11 -0500

    Exclude 24 bit audio

--- projects/urbanSoundsSorter.py
+++ projects/urbanSoundsSorter.py
@@ -1,5 +1,6 @@
 import os
 import re
+from scipy.io import wavfile
 
 classes = ['air_conditioner', 'car_horn', 'children_playing', 
            'dog_bark', 'drilling', 'engine_idling', 'gun_shot',
@@ -26,6 +27,10 @@ for c in range(0,10):
 
         #Iterates through all the files in the folder
         for file in fileList:
-            if re.search(pattern, file):
-                # move file from to folder
-                os.rename(sourceFolder + file, destFolder + file)
+            try:
+                sampling_rate, signal = wavfile.read(sourceFolder + file)
+                if re.search(pattern, file) and sampling_rate != 24:
+                    # move file from to folder
+                    os.rename(sourceFolder + file, destFolder + file)
+            except:
+                pass
